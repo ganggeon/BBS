f8f86015ba7140a47cf34dd0fef1689ba4ac977d
ganggeon <62833757+ganggeon@users.noreply.github.com>
2020-04-25 03:45:34 +0900

    comment update

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -8,11 +8,11 @@ import java.sql.ResultSet;
 
 public class UserDAO {
 
-	private Connection conn;
-	private PreparedStatement pstmt;  
-	private ResultSet rs;
-	
-	public UserDAO() { //mySQL에 접속 가능하게 해준다
+	private Connection conn; //connection: DB에접근하게 해주는 객체
+	private PreparedStatement pstmt; // prepared statement: 정해진 sql문장을 db에 삽입하는 형식으로 인스턴스를 가져온다
+	private ResultSet rs; //ResultSet: DB data를 담을 수 있는 객체
+	//DB에 접속 가능하게 해준다
+	public UserDAO() { // 생성자 실행될때마다 자동으로 DB연결이 이루어 질 수 있도록 한다
 		try {
 			String dbURL = "jdbc:mysql://localhost:3306/gun5752?characterEncoding=UTF-8&serverTimezone=UTC"; 
 			String dbID = "gun5752";
@@ -24,13 +24,13 @@ public class UserDAO {
 		}
 	}
 	
-	public int login(String userID, String userPassword) {
+	public int login(String userID, String userPassword) { //로그인 함수
 		
-      String SQL = "SELECT userPassword FROM USER WHERE userID = ?"; // user테이블에서 데이터를 가져온다
+      String SQL = "SELECT userPassword FROM USER WHERE userID = ?"; //user테이블에서 데이터를 가져오는 쿼리문
       try {
-    	  pstmt = conn.prepareStatement(SQL);
-    	  pstmt.setString(1, userID);
-    	  rs = pstmt.executeQuery();
+    	  pstmt = conn.prepareStatement(SQL); // DB에 접근해서 sql문을 삽입한다
+    	  pstmt.setString(1, userID); // 첫번째 인덱스의 파라미터 값(쿼리문의 ?)을 userID로 지정한다.
+    	  rs = pstmt.executeQuery(); // rs에 결과값 보관
     	  
     	  if (rs.next()) {
     		  if(rs.getString(1).equals(userPassword)) {
